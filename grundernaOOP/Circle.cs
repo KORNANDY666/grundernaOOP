1e5ed13d1c9611a1ffbd9eeac4f6f89da09e389b
KORNANDY666 <113092594+KORNANDY666@users.noreply.github.com>
2022-10-03 09:47:13 +0200

    Add project files.

---
+++ grundernaOOP/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grundernaOOP
+{
+    internal class Circle
+    {
+        // Creates variables to do calculations with.
+        float pi = 3.141f;
+        int radius;
+
+
+
+        // creates a constructor to take the value of the radius
+        public Circle(int Radius)
+        {
+          radius = Radius;
+           
+        }
+
+
+        // 2 methods to calculate 
+        public void getArea()
+        {
+            Console.WriteLine(radius * radius * pi ); 
+           
+        }
+        public void getVolume()
+        {
+            Console.WriteLine((4 * pi * radius * radius * radius) /3);
+        }
+    }
+}
---
+++ grundernaOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace grundernaOOP                                     // Andreas Blom YH.Net-22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates 3 different circles with different radius values ​​to calculate the area and volume
+
+
+            Circle circle = new Circle(5);
+            Circle circle2 = new Circle(6);
+            circle.getArea();
+            circle2.getArea();
+
+
+            Circle circle3 = new Circle(11);
+            circle3.getVolume();
+        }
+    }
+}
